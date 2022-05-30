731abf0071b102cfa3ad525a8c13b7cb456906a8
aayusha-aigel <aayush.adhikari@aigeal.com>
2022-05-30 08:03:26 +0545

    Category

--- home/models.py
+++ home/models.py
@@ -1,3 +1,56 @@
 from django.db import models
-
+STATUS = (('active','Active'),('','Default'))
+LABELS = (('offer','Offer'),('new','New'),('hot','Hot'),('','Default'))
 # Create your models here.
+class Category(models.Model):
+	name = models.CharField(max_length = 300)
+	slug = models.CharField(max_length = 500, unique = True)
+	image = models.ImageField(upload_to = 'media', null = True)
+
+	def __str__(self):
+		return self.name
+
+class SubCategory(models.Model):
+	name = models.CharField(max_length = 300)
+	slug = models.CharField(max_length = 500, unique = True)
+	category = models.ForeignKey(Category,on_delete = models.CASCADE)
+
+	def __str__(self):
+		return self.name
+
+class Slider(models.Model):
+	name = models.CharField(max_length = 300)
+	url = models.URLField(max_length = 500)
+	image = models.ImageField(upload_to = 'media', null = True)
+	rank = models.IntegerField()
+	status = models.CharField(choices = STATUS,max_length = 300,blank = True)
+
+	def __str__(self):
+		return self.name
+
+class Ad(models.Model):
+	name = models.CharField(max_length = 300)
+	url = models.URLField(max_length = 500)
+	image = models.ImageField(upload_to = 'media', null = True)
+	rank = models.IntegerField()
+	status = models.CharField(choices = STATUS,max_length = 300,blank = True)
+
+	def __str__(self):
+		return self.name
+
+class Product(models.Model):
+	name = models.CharField(max_length = 300)
+	slug = models.CharField(max_length = 500, unique = True)
+	image = models.ImageField(upload_to = 'media', null = True)
+	rank = models.IntegerField()
+	status = models.CharField(choices = STATUS,max_length = 300,blank = True)
+	price = models.FloatField()
+	discounted_price = models.FloatField()
+	category = models.ForeignKey(Category,on_delete = models.CASCADE)
+	subcategory = models.ForeignKey(SubCategory,on_delete = models.CASCADE)
+	description = models.TextField(blank = True)
+	labels = models.CharField(choices = LABELS,max_length = 300,blank = True)
+
+	def __str__(self):
+		return self.name
+
--- home/views.py
+++ home/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.views import View
 
+from .models import *
 # Create your views here.
+
+class BaseView(View):
+	views = {}
+	views['categories'] = Category.objects.all()
+	views['subcategories'] = SubCategory.objects.all()
+
+
+
+class HomeView(BaseView):
+	def get(self,request):
+		self.views['new_products'] = Product.objects.filter(labels = 'new')
+		self.views['hot_products'] = Product.objects.filter(labels = 'hot')
+		self.views['offer_products'] = Product.objects.filter(labels = 'offer')
+		self.views['sliders'] = Slider.objects.all()
+		self.views['ads'] = Ad.objects.all()
+		return render(request,'index.html',self.views)
+
